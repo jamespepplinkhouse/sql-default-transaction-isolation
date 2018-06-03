8b644b03ce09e51f9bead2edd06a463fddf70cf3
James Pepplinkhouse <james.pepplinkhouse@gmail.com>
2018-06-03 15:37:13 +1000

    ...

--- src/getTestQueries.ts
+++ src/getTestQueries.ts
@@ -3,36 +3,51 @@ import sampleProducts from './database/sampleProducts'
 
 export default (sequelize, models) => {
   const lowerBlankaQuantity = async () => {
-    await sequelize.transaction(async transaction => {
-      const blanka = await models.Product.findOne({
-        where: { name: 'Blanka' },
-        lock: transaction.LOCK.UPDATE,
-        transaction
-      })
-      blanka.quantity--
+    await sequelize.transaction(
+      {
+        isolationLevel: Sequelize.Transaction.ISOLATION_LEVELS.READ_COMMITTED
+      },
+      async transaction => {
+        const blanka = await models.Product.findOne({
+          where: { name: 'Blanka' },
+          lock: transaction.LOCK.UPDATE,
+          transaction
+        })
+        blanka.quantity--
 
-      // Do some other IO
-      await new Promise(res => {
-        setTimeout(res, 50)
-      })
+        // Do some other IO
+        await new Promise(res => {
+          setTimeout(res, 50)
+        })
 
-      await blanka.save({ transaction })
-    })
+        await blanka.save({ transaction })
+      }
+    )
   }
 
   const findRandomProduct = async () => {
     const name =
       sampleProducts[Math.floor(Math.random() * sampleProducts.length)].name
 
-    await sequelize.transaction(async transaction => {
-      await models.Product.findOne({ where: { name }, transaction })
-    })
+    await sequelize.transaction(
+      {
+        isolationLevel: Sequelize.Transaction.ISOLATION_LEVELS.READ_UNCOMMITTED
+      },
+      async transaction => {
+        await models.Product.findOne({ where: { name }, transaction })
+      }
+    )
   }
 
   const findAllProducts = async () => {
-    await sequelize.transaction(async transaction => {
-      const products = await models.Product.findAll({ transaction })
-    })
+    await sequelize.transaction(
+      {
+        isolationLevel: Sequelize.Transaction.ISOLATION_LEVELS.READ_UNCOMMITTED
+      },
+      async transaction => {
+        const products = await models.Product.findAll({ transaction })
+      }
+    )
   }
 
   return [findRandomProduct, findAllProducts, lowerBlankaQuantity]
